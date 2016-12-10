db6c789ceb59e7bb273430f12d6b2df8a80d8e94
Robert Ng <robertng85@gmail.com>
2016-12-09 19:39:35 -0500

    shutterstock api set up

--- services/images.js
+++ services/images.js
@@ -1,19 +1,21 @@
-// const fetch = require('node-fetch');
+const fetch = require('node-fetch');
 // const API_URL = "https://api.shutterstock.com/v2";
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
 
-// function searchImages(req, res, next) {
-//   console.log(req.query);
-//   fetch(`${API_URL}t=${req.query.movies}`)
-//   .then(results => results.json())
-//   .then((data) => {
-//     res.movies = data;
-//     next();
-//   })
-//   .catch((err) => {
-//     res.err = err;
-//     next();
-//   });
-// }
+function searchImages(req, res, next) {
+  console.log(req.query);
+  fetch(`https://${client_id}:${client_secret}@api.shutterstock.com/v2/images/search?query=${req.query.images}`)
+  .then(results => results.json())
+  .then((data) => {
+    res.images = data;
+    next();
+  })
+  .catch((err) => {
+    res.err = err;
+    next();
+  });
+}
 
-// module.exports = { searchMovies };
+module.exports = { searchImages };
 
